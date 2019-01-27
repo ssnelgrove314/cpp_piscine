a1ddfec820e70fc206c8f655e35f4a8aea92f902
scott.snelgrove1@gmail.com <scott.snelgrove1@gmail.com>
2019-01-27 14:03:28 -0800

    test

--- Rush00/inc/GameObject.class.hpp
+++ Rush00/inc/GameObject.class.hpp
@@ -1,17 +1,20 @@
 #ifndef GAME_OBJECT_CLASS_HPP
 #define GAME_OBJECT_CLASS_HPP
 
+struct t_coord
+{
+    int x;
+    int y;
+}; 
 class GameObject {
-    int g_row;
-    int g_col;
     bool g_alive;
     char g_ch;
+    t_coord &loc;
 
     public:
         GameObject(int row, int col, char ch, char alive);
         ~GameObject();
-        int *get_loc();
-        
+        t_coord &get_loc();
 };
 
 #endif
